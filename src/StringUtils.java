1c62b5386745c2e5ed367fe320b254ef31e99f0d
vandewak <42846101+vandewak@users.noreply.github.com>
2018-09-29 12:51:44 -0400

    Update StringUtils.java
    I was unable to directly update the files from within IntellJ, so I just copy-pasted the working code directly onto GitHub.

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -2,7 +2,7 @@ public class StringUtils {
     /**
      * makePurdueUsername -- <= 8-letter-long lowercase username creator
      * Usernames may be AT MOST 8 letters in length, and should
-     * consist of the first letter of one's first name, and the 
+     * consist of the first letter of one's first name, and the
      * amount of their last name that will fit into the remaining
      * 7 characters. All should be lowercase.
      * Ex: a = "Sean ", b = "Flannery", returns "sflanner"
@@ -17,12 +17,14 @@ public class StringUtils {
             Ex: res = "SFlannery"
                     --> res = "SFlanner"
          */
-        res.substring(0, 8);
+        if (res.length() > 8) {
+            res = res.substring(0, 8);
+        }
         /* Convert the name to lower-case
             Ex: res = "SFlanner"
                     --> res = "sflanner"
          */
-        res.toLowerCase();
+        res = res.toLowerCase();
         /* return the result of our calculation */
         return res;
     }
@@ -30,12 +32,12 @@ public class StringUtils {
     /**
      * replaceStudentUsername -- replace student usernames in sensitive text
      * We want to make sure student information is expunged from sensitive
-     * documents as much as possible. 
+     * documents as much as possible.
      * Ex: text = "jframes was among the students whose SSNs were leaked.",
      * username = "[DATA EXPUNGED] was among the students whose SSNs were leaked.",
      */
     public static String replaceStudentUsername(String text, String username) {
         /* Replace every instance of the username with proper message */
-        return text.replaceAll(username, "[DATA EXPUNDGED]");
+        return text.replaceAll(username, "[DATA EXPUNGED]");
     }
 }
